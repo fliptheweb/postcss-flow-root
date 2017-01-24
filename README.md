d54fe6c90782fb82aaf7e5b8c6d9382fc8364acf
fliptheweb <itisshortemailaddress@gmail.com>
2017-01-25 00:35:59 +0700

    feat(): support display: flow-root

---
+++ README.md
@@ -0,0 +1,3 @@
+## display: flow-root
+Article https://rachelandrew.co.uk/archives/2017/01/24/the-end-of-the-clearfix-hack/
+Editor’s Draft of the CSS Display Module Level 3 https://drafts.csswg.org/css-display/#valdef-display-flow-root
---
+++ index.js
@@ -0,0 +1,21 @@
+var postcss = require('postcss');
+ 
+module.exports = postcss.plugin('postcss-display-flow-root', function(options) {
+  options = options || {};
+  
+  return function(root) {
+    root.walkRules(function(rule) {
+      // NOTE: nothing to do, if element have overflow:hidden cuz it create own flow
+      haveOverflowHidden = false
+      rule.walkDecls('overflow', function(decl) {
+        if (decl.value == 'hidden') { haveOverflowHidden = true; }
+      })
+      rule.walkDecls('display', function(decl) {
+        if (decl.value == 'flow-root' && !haveOverflowHidden) {
+          // NOTE: last clearfix version (IE8+) https://css-tricks.com/snippets/css/clear-fix/
+          root.append(rule.selector + '::after {content: "";display: table;clear: both;}');
+        }
+      });
+    });
+  }
+});
