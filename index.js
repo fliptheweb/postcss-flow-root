959dac22c0691b5bcf0319c855b4fccc58679502
Mathias Bynens <mathias@qiwi.be>
2017-01-25 09:27:32 +0100

    Add missing `var`

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ module.exports = postcss.plugin('postcss-display-flow-root', function(options) {
   return function(root) {
     root.walkRules(function(rule) {
       // NOTE: nothing to do, if element have overflow:hidden cuz it create own flow
-      haveOverflowHidden = false
+      var haveOverflowHidden = false;
       rule.walkDecls('overflow', function(decl) {
-        if (decl.value == 'hidden') { haveOverflowHidden = true; }
-      })
+        if (decl.value == 'hidden') {
+          haveOverflowHidden = true;
+        }
+      });
       rule.walkDecls('display', function(decl) {
         if (decl.value == 'flow-root' && !haveOverflowHidden) {
           // NOTE: last clearfix version (IE8+) https://css-tricks.com/snippets/css/clear-fix/
